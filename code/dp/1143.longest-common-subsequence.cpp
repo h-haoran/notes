b37c2fa5ed3a60d7c1d55e6d939df1acbd2ce7d0
Alon <haoranhuang1997@gmail.com>
2023-04-24 03:42:25 +1000

    1143

---
+++ code/dp/1143.longest-common-subsequence.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1143 lang=cpp
+ *
+ * [1143] Longest Common Subsequence
+ */
+
+// @lc code=start
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n =  text1.size(), m = text2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ( text1[i] == text2[j]) {
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                } else {
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+// @lc code=end
+
